339851a3f8d724184ee3cf6b82532b120f080204
Ilya Gorsky <gorsky.art@gmail.com>
2020-05-02 19:18:43 +0300

    Readonly type

---
+++ example/typesdata/readonly.ts
@@ -0,0 +1,34 @@
+/**
+ * Readonly
+ * Метод доступа и чтения свойства
+ * @module readonly.ts
+ */
+interface User {
+  readonly type: string;
+  name: string;
+}
+
+/**
+ * Readonly object field
+ */
+var UserApplicant: User = {
+  type: "applicant",
+};
+
+UserApplicant.type = "admin";
+
+var protectedUserApplicant: Readonly<User> = {
+  type: "applicant",
+  name: "Ilya",
+};
+
+protectedUserApplicant.name = "d";
+
+/**
+ * Readonly arrays
+ */
+var abc: readonly string[] = ["a", "b", "c"];
+var abc: Readonly<Array<string>> = ["a", "b", "c"];
+var abc: ReadonlyArray<string> = ["a", "b", "c"];
+
+abc[0] = "d";
