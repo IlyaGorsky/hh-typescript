5bc7fff2f480aa0a91d36ac50ab3b8113dd20a91
Ilya Gorsky <gorsky.art@gmail.com>
2020-05-04 15:15:22 +0300

    Type assertion add new examples

--- example/typesdata/assertion.ts
+++ example/typesdata/assertion.ts
@@ -7,7 +7,7 @@
  */
 var helloWorldString: any = "Hello world";
 
-// var helloWorldStringLen: object = (helloWorldString as Array<any>).pop();
+// var helloWorldStringLength = (helloWorldString as Array<any>).pop();
 
 /**
  * Cпособ 1
@@ -18,3 +18,11 @@ var helloWorldStringLength: number = (<string>helloWorldString).length;
  * Cпособ 2
  */
 var helloWorldStringLength: number = (helloWorldString as string).length;
+
+type Baz = { a: number; b: number };
+let v1 = { a: 1, b: 2 } as Baz;
+let v2 = { a: 1 } as Baz; // Супер тип
+let v3 = { a: 1, b: 2, c: 3 } as Baz; // Подтип
+// let v4 = { d: 4 } as Baz; // bad
+
+let forms = document.querySelectorAll(".form") as NodeListOf<HTMLFormElement>;
